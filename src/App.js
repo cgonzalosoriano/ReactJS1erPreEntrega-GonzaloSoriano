23891a0d79a61f3adeb31d81197aacf547b63463
Gonzalo Soriano <cgonzalosoriano@gmail.com>
2024-07-01 20:23:04 -0300

    agrego cambios 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import React from 'react';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-        <NavBar/>
-        <ItemListContainer greeting={"Bienvenidos!!"}/>
+      <NavBar />
+      <ItemListContainer greeting="Bienvenido a nuestra tienda!" />
     </div>
   );
 }
